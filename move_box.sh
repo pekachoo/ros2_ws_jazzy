7f3734fd067205831a12aeddad42fe97f16dea09
pekachoo <jliu6162@gmail.com>
2025-04-13 11:46:32 -0400

    dynamic param adjusting works

--- move_box.sh
+++ move_box.sh
@@ -14,7 +14,7 @@ move_box() {
         --reptype gz.msgs.Boolean \
         --timeout 1000 \
         --req "name: \"$name\", position: {x: $x, y: $y_pos, z: 0.25}, orientation: {w: 1.0}"
-      sleep 0.05
+      sleep 0.2
     done
     for y_pos in $(seq 0.5 -0.1 -6.5); do
       gz service -s /world/empty/set_pose \
@@ -22,7 +22,7 @@ move_box() {
         --reptype gz.msgs.Boolean \
         --timeout 1000 \
         --req "name: \"$name\", position: {x: $x, y: $y_pos, z: 0.25}, orientation: {w: 1.0}"
-      sleep 0.05
+      sleep 0.2
     done
   else
     for y_pos in $(seq "$y" -0.1 -6.5); do
@@ -31,7 +31,7 @@ move_box() {
         --reptype gz.msgs.Boolean \
         --timeout 1000 \
         --req "name: \"$name\", position: {x: $x, y: $y_pos, z: 0.25}, orientation: {w: 1.0}"
-      sleep 0.05
+      sleep 0.2
     done
     for y_pos in $(seq -6.5 0.1 0.5); do
       gz service -s /world/empty/set_pose \
@@ -39,13 +39,12 @@ move_box() {
         --reptype gz.msgs.Boolean \
         --timeout 1000 \
         --req "name: \"$name\", position: {x: $x, y: $y_pos, z: 0.25}, orientation: {w: 1.0}"
-      sleep 0.05
+      sleep 0.2
     done
   fi
 }
 
 while true; do
-  # run in parallel so all boxes move at the same time
   move_box "moving_box_1" 2 -2 down &
   move_box "moving_box_2" 5 -3 up &
   move_box "moving_box_3" 8 -5 down &
